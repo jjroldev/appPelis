edddb198f0d7da1bcfaa197834973b75b0316068
Justyn Roldan <justinjaelroldanmendez123@gmail.com>
2024-12-24 20:26:28 -0500

    react-multi-carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
-import { CardMovie } from './components/CardMovie/CardMovie'
-import { Movie } from './interface/Movie'
-export const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import "react-multi-carousel/lib/styles.css";
+import { Movie } from "./interface/Movie";
+import { MovieSwiper } from "./components/MovieSwiper/MovieSwiper";
+export const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([])
-  const API_URL = 'https://api.themoviedb.org/3'
-  const API_KEY = '6e6a632ff00b90c42dadea3c48a464ab'
-  const [logoPath,setLogoPath]=useState('');
-  const fecthMovies = async (searchKey: string) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "6e6a632ff00b90c42dadea3c48a464ab";
+  const [logoPath, setLogoPath] = useState("");
+
+  const fetchMovies = async (searchKey: string) => {
     try {
-      const type = searchKey ? 'search' : 'discover'
-      const { data: { results } } = await axios.get(`${API_URL}/${type}/movie`, {
+      const type = searchKey ? "search" : "discover";
+      const {
+        data: { results },
+      } = await axios.get(`${API_URL}/${type}/movie`, {
         params: {
           api_key: API_KEY,
           query: searchKey,
-        }
-      })
-      setMovies(results)
-      console.log(results)
+        },
+      });
+      setMovies(results);
     } catch {
-      console.error("error")
+      console.error("Error fetching movies");
     }
-
-
-  }
+  };
 
   const fetchMovieImages = async (movieId: number) => {
     try {
@@ -34,49 +36,54 @@ function App() {
           api_key: API_KEY,
         },
       });
-      const logo = data.logos.find((logo: any) => logo.iso_639_1 === "en") || data.logos[0];
-      setLogoPath(logo.file_path)
+      const logo =
+        data.logos.find((logo: any) => logo.iso_639_1 === "en") ||
+        data.logos[0];
+      setLogoPath(logo.file_path);
     } catch (error) {
-      console.error("Error al obtener las imágenes de la película:", error);
+      console.error("Error fetching movie images:", error);
     }
   };
-  
 
   useEffect(() => {
-    fecthMovies("")    
+    fetchMovies("us");
   }, []);
 
   useEffect(() => {
     if (movies.length > 0) {
-      fetchMovieImages(movies[0].id)
+      fetchMovieImages(movies[0].id);
     }
-  }, [movies])
-
+  }, [movies]);
 
   return (
     <>
-      <div className="header" >
+      <div className="header">
         <div className="navbar">
-          <img src="../src/images/Netflix_Logo_RGB.png" alt="logo-de-netflix-practica" />
+          <img
+            src="../src/images/Netflix_Logo_RGB.png"
+            alt="Netflix Logo"
+          />
           <i className="fa-solid fa-magnifying-glass"></i>
         </div>
         {movies.length > 0 && (
           <>
-          <img className='fondo' src={URL_IMAGE + movies[0].backdrop_path} alt={movies[0].title} />
-          <div className='contenedorLogo'>
-            <img src={URL_IMAGE+logoPath} alt="" />
-            <p>{movies[0].overview}</p>
-          </div>
+            <img
+              className="fondo"
+              src={URL_IMAGE + movies[0].backdrop_path}
+              alt={movies[0].title}
+            />
+            <div className="contenedorLogo">
+              <img src={URL_IMAGE + logoPath} alt="" />
+              <p>{movies[0].overview}</p>
+            </div>
           </>
         )}
       </div>
       <div className="contenedorPeliculas">
-        {movies.map((movie) => (
-          <CardMovie key={movie.id} movie={movie} />
-        ))}
+        <MovieSwiper movies={movies}/>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MovieSwiper/MovieSwiper.tsx
@@ -0,0 +1,40 @@
+import { Movie } from "../../interface/Movie";
+import Carousel from "react-multi-carousel";
+import { CardMovie } from "../CardMovie/CardMovie";
+export function MovieSwiper({movies}:{movies:Movie[]}){
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 3,
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2,
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1,
+        },
+      };
+    return(
+        <Carousel
+          responsive={responsive}
+          autoPlay
+          infinite
+          showDots
+          swipeable
+          draggable
+          renderArrowsWhenDisabled={false}
+        >
+          {movies.map(
+            (movie) =>
+              movie.poster_path && (
+                <CardMovie key={movie.id} movie={movie} />
+              )
+          )}
+        </Carousel>
+    )
+}
