acfbe89ff2012a8e56f6de0baa72d637b03510f2
Justyn Roldan <justinjaelroldanmendez123@gmail.com>
2025-01-06 18:50:51 -0500

    Updates

--- assets/Poster-Da6MJzQr.js
+++
@@ -1,1 +0,0 @@
-import{j as s,e}from"./index-DbnsQP8H.js";function t({poster_path:r}){return s.jsx("div",{className:"wrapperPoster",children:s.jsx("img",{src:e+r,className:"backdropPoster"})})}export{t as default};
---
+++ assets/Poster-Du14e8NG.js
@@ -0,0 +1,1 @@
+import{j as s,e}from"./index-CqiBNT2y.js";function t({poster_path:r}){return s.jsx("div",{className:"wrapperPoster",children:s.jsx("img",{src:e+r,className:"backdropPoster"})})}export{t as default};
