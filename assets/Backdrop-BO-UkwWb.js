20d511f77252876d45b15d93af8a72e5b21e4660
Justyn Roldan <justinjaelroldanmendez123@gmail.com>
2025-01-06 18:55:55 -0500

    Updates

---
+++ assets/Backdrop-BO-UkwWb.js
@@ -0,0 +1,1 @@
+import{j as r,e as s}from"./index-CAcqTVvU.js";function c({backdrop_path:a}){return r.jsx(r.Fragment,{children:r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})})}export{c as default};
