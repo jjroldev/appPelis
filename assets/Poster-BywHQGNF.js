7644ca5a8f134bc94bf1b438a8bdd903229583e1
Justyn Roldan <justinjaelroldanmendez123@gmail.com>
2025-01-11 19:33:15 -0500

    Updates

---
+++ assets/Poster-BywHQGNF.js
@@ -0,0 +1,1 @@
+import{j as s,e}from"./index-FBMCfUL2.js";function t({poster_path:r}){return s.jsx("div",{className:"wrapperPoster",children:s.jsx("img",{src:e+r,className:"backdropPoster"})})}export{t as default};
