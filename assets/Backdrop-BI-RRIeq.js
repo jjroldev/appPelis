083546dc39e0d86d3b6505d81ea6593991b2e617
Justyn Roldan <justinjaelroldanmendez123@gmail.com>
2025-01-12 18:42:49 -0500

    Updates

--- assets/Backdrop-BI-RRIeq.js
+++
@@ -1,1 +0,0 @@
-import{j as r,d as s}from"./index-D4TGrLJa.js";function c({backdrop_path:a}){return r.jsx(r.Fragment,{children:r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})})}export{c as default};
