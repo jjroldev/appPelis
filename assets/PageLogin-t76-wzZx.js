48147afdd16b646770128a69069234bd03127a50
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-06 18:46:57 -0500

    Updates

---
+++ assets/PageLogin-t76-wzZx.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-nLxA1X93.js";import{B as r}from"./Base-DeV6otX9.js";import"./NavBar-CxuLc1AR.js";function e(){return o.jsx(r,{isLogin:!0})}export{e as default};
--- assets/PageRegister-DQqggpaV.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ddgygG09.js";import{B as t}from"./Base-DWHe7VxX.js";import"./NavBar-BIb9Jzd-.js";function a(){return r.jsx(t,{})}export{a as default};
