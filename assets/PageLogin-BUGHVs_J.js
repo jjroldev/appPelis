e65364dd69341e0dc2361542d3a254ec42d34c4f
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-07 18:09:42 -0500

    Updates

---
+++ assets/PageLogin-BUGHVs_J.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Base-DRTW-DJP.js","assets/index-CPnFDXjM.js","assets/index-Dv0mkcXI.css","assets/NavBar-DwybwRdf.js","assets/NavBar-Yi_GCoUL.css","assets/Base-B-nKZwAG.css"])))=>i.map(i=>d[i]);
+import{j as e,r as s,S as r,_ as t}from"./index-CPnFDXjM.js";const a=s.lazy(()=>t(()=>import("./Base-DRTW-DJP.js"),__vite__mapDeps([0,1,2,3,4,5])));function i(){return e.jsx(s.Suspense,{fallback:e.jsx(r,{}),children:e.jsx(a,{isLogin:!0})})}export{i as default};
