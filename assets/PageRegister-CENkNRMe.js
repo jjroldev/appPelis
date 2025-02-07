388edcb994183fec6d642283258a0ed726aa66ea
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-07 17:18:11 -0500

    Updates

---
+++ assets/PageRegister-CENkNRMe.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Base-Aex9Gp-f.js","assets/index-BwtgmDVT.js","assets/index-Dv0mkcXI.css","assets/NavBar-DN4l7E8j.js","assets/NavBar-Yi_GCoUL.css","assets/Base-B-nKZwAG.css"])))=>i.map(i=>d[i]);
+import{j as e,r as s,S as r,_ as t}from"./index-BwtgmDVT.js";const a=s.lazy(()=>t(()=>import("./Base-Aex9Gp-f.js"),__vite__mapDeps([0,1,2,3,4,5])));function i(){return e.jsx(s.Suspense,{fallback:e.jsx(r,{}),children:e.jsx(a,{})})}export{i as default};
