582f65da35450adb62cb22a2162d540e2c9a770f
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-07 16:04:20 -0500

    Updates

--- assets/PageRegister-BKcba3J5.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BAD-sRFl.js";import{B as t}from"./Base-CESRznxn.js";import"./NavBar-DU7vfCct.js";function a(){return r.jsx(t,{})}export{a as default};
---
+++ assets/PageRegister-D3l-NJKF.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C6WLrO6e.js";import{B as t}from"./Base-DQALdMEl.js";import"./NavBar-CpVsUITX.js";function a(){return r.jsx(t,{})}export{a as default};
