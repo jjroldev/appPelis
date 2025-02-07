3b06f218fcc1482ddcc3d2127fed0f05c0c155d6
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-07 16:20:57 -0500

    Updates

---
+++ assets/PageRegister-DA44h-q-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Base-CODOnR2t.js","assets/index-KBTqFSRM.js","assets/index-Dv0mkcXI.css","assets/NavBar-DpPQbjHs.js","assets/NavBar-Yi_GCoUL.css","assets/Base-B-nKZwAG.css"])))=>i.map(i=>d[i]);
+import{j as e,r as s,S as r,_ as t}from"./index-KBTqFSRM.js";const a=s.lazy(()=>t(()=>import("./Base-CODOnR2t.js"),__vite__mapDeps([0,1,2,3,4,5])));function i(){return e.jsx(s.Suspense,{fallback:e.jsx(r,{}),children:e.jsx(a,{})})}export{i as default};
