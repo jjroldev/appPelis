b17990eac75358337c2bf8655944a265ac7be22b
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-07 18:16:05 -0500

    Updates

---
+++ assets/PageLogin-C2qYAgTy.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Base-CHz-M6-5.js","assets/index-CyazyFac.js","assets/index-Dv0mkcXI.css","assets/NavBar-DcEZoaYs.js","assets/NavBar-Yi_GCoUL.css","assets/Base-B-nKZwAG.css"])))=>i.map(i=>d[i]);
+import{j as e,r as s,S as r,_ as t}from"./index-CyazyFac.js";const a=s.lazy(()=>t(()=>import("./Base-CHz-M6-5.js"),__vite__mapDeps([0,1,2,3,4,5])));function i(){return e.jsx(s.Suspense,{fallback:e.jsx(r,{}),children:e.jsx(a,{isLogin:!0})})}export{i as default};
---
+++ assets/Poster-Wy7t2A80.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CyazyFac.js";import{a}from"./endPoints-D6Ym8Q8U.js";function t({poster_path:s}){return r.jsx("div",{className:"wrapperPoster",children:r.jsx("img",{src:a+s,className:"backdropPoster"})})}export{t as default};
