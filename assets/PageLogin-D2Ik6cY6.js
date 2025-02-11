34e110d5a18f0dd6ad04b3aad070b3372e2ddb40
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-10 21:02:39 -0500

    Updates

--- assets/PageLogin-D2Ik6cY6.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Base-rd1QF9TA.js","assets/index-CKgC-fQJ.js","assets/index-BDcjLdJv.css","assets/NavBar-B3SP4niS.js","assets/NavBar-CacxovuT.css","assets/Base-CxjNw1pq.css"])))=>i.map(i=>d[i]);
-import{j as e,r as s,S as r,_ as t}from"./index-CKgC-fQJ.js";const a=s.lazy(()=>t(()=>import("./Base-rd1QF9TA.js"),__vite__mapDeps([0,1,2,3,4,5])));function i(){return e.jsx(s.Suspense,{fallback:e.jsx(r,{}),children:e.jsx(a,{isLogin:!0})})}export{i as default};
