e1011bb2538ba9999e84384f609f29e7f1e22fd9
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-12 01:25:52 -0500

    Updates

---
+++ assets/Backdrop-Bdwshl2v.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DGcxzClU.js";import{d as s}from"./CardMovie--HN_EPjx.js";function c({backdrop_path:a}){return r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})}export{c as default};
