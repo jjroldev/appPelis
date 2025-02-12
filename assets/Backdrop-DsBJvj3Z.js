8c89e27850e13864adf95761926d83b806a89d78
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-12 00:42:29 -0500

    Updates

---
+++ assets/Backdrop-DsBJvj3Z.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CUXnXWV-.js";import{d as s}from"./CardMovie-Df_zACt-.js";function c({backdrop_path:a}){return r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})}export{c as default};
