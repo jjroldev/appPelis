7f4ee66220f6bf0507231adf0abc55b4c7b5bfd9
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-17 17:29:05 -0500

    Updates

---
+++ assets/PageRegister-Ce4-HozH.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Base-he1ELFJD.js","assets/index-I0KVTuNB.js","assets/index-BRG9CBly.css","assets/NavBar-DmpPlhS9.js","assets/useQuery-GHSVvEtE.js","assets/NavBar-DDov0PtD.css","assets/Base-D8e0nx2y.css"])))=>i.map(i=>d[i]);
+import{j as e,r as s,S as r,_ as t}from"./index-I0KVTuNB.js";const a=s.lazy(()=>t(()=>import("./Base-he1ELFJD.js"),__vite__mapDeps([0,1,2,3,4,5,6])));function i(){return e.jsx(s.Suspense,{fallback:e.jsx(r,{}),children:e.jsx(a,{})})}export{i as default};
