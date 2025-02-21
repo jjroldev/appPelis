193794c24d3e43a052ffd0b40782a21a40f26e47
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-21 14:57:50 -0500

    Updates

---
+++ assets/Backdrop-LMDKCztQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-cHxvsVQy.js";import{q as s}from"./BarMenu-DHwZNA8j.js";function c({backdrop_path:a}){return r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})}export{c as default};
