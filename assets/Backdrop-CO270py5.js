ee2bb3b6c7680fc455d21f132076d4f1fc267f1b
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-20 22:57:05 -0500

    Updates

--- assets/Backdrop-CO270py5.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BPvWBwkq.js";import{q as s}from"./BarMenu-BUas4uG-.js";function c({backdrop_path:a}){return r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})}export{c as default};
---
+++ assets/Backdrop-I3XU3JSM.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C3xqu_lt.js";import{q as s}from"./BarMenu-UIAJhvM1.js";function c({backdrop_path:a}){return r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})}export{c as default};
