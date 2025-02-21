2ab53f2e27de7084f18841f762e07242ad8f4b0a
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-20 19:09:06 -0500

    Updates

---
+++ assets/PageRegister-DtfeibLw.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Base-qrQziPLe.js","assets/index-Bv7YEbXX.js","assets/index-C_6mexaa.css","assets/Logo-C85KNIFn.js","assets/index-Dcx_4SdG.js","assets/Logo-WBtp3qC0.css","assets/Base-D8e0nx2y.css"])))=>i.map(i=>d[i]);
+import{j as e,r as s,S as r,_ as t}from"./index-Bv7YEbXX.js";const a=s.lazy(()=>t(()=>import("./Base-qrQziPLe.js"),__vite__mapDeps([0,1,2,3,4,5,6])));function i(){return e.jsx(s.Suspense,{fallback:e.jsx(r,{}),children:e.jsx(a,{})})}export{i as default};
---
+++ assets/TransitionGroupContext-BfVqEgQM.js
@@ -0,0 +1,1 @@
+import{R as t}from"./index-Bv7YEbXX.js";const e=t.createContext(null);export{e as T};
