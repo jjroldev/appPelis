fe156f17e7fbcf34ded3f5c83ba8de4efa52769c
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-02-25 15:26:18 -0500

    Updates

--- assets/Backdrop-CU_t0DCU.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ZhOadw6B.js";import{x as s}from"./fetchData-D8ToqI1N.js";function c({backdrop_path:a}){return r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})}export{c as default};
