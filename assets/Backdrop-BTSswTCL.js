2d3033f5864873f4569a1c9bdba84424b51b0ac9
jjroldev <justinjaelroldanmendez123@gmail.com>
2025-03-06 15:26:03 -0500

    Updates

---
+++ assets/Backdrop-BTSswTCL.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Bdt1_3wt.js";import{y as s}from"./fetchData-NBeNkyoS.js";function c({backdrop_path:a}){return r.jsx("div",{className:"wrapperBackdrop",children:r.jsx("img",{src:s+a,className:"backdropInfo"})})}export{c as default};
---
+++ assets/TransitionGroupContext-Cl_pzzgw.js
@@ -0,0 +1,1 @@
+import{R as t}from"./index-Bdt1_3wt.js";const e=t.createContext(null);export{e as T};
